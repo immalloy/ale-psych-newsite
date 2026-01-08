21a1815178b270fa4d58c9aac268d0b3a0847c6f
Malloy <lefehanims@gmail.com>
2026-01-08 16:13:08 -0600

    old mods

---
+++ OLD SITE SOURCE/mods/mods-data.js
@@ -0,0 +1,10 @@
+const MOD_FOLDERS = [
+  { slug: "trap-4-silly-kids", categories: ["featured", "original"] },
+  { slug: "vs-guy-named-laumber-mobile", categories: ["featured", "original"] },
+  { slug: "ale-modidregistry", categories: ["tools"] },
+  { slug: "vs-memstar", categories: ["upcoming", "featured", "original"] },
+  { slug: "friday-night-calvin", categories: ["upcoming", "featured", "original"] },
+  { slug: "vs-roaring-knight", categories: ["upcoming", "original"] },
+  { slug: "rappin-every-friday", categories: ["upcoming", "original"] },
+  { slug: "secret-exit-the-lost-levels", categories: ["upcoming", "original"] }
+];
